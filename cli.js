ff2ba4b9cd15d5dd2b5664e3977e2373c5de3249
pengzekuan <1767575509@qq.com>
2017-05-10 11:50:50 +0800

    入口输出修改

--- cli.js
+++ cli.js
@@ -7,10 +7,9 @@ var child_process = require('child_process');
 child_process.execFile(path.join(__dirname, 'bin', `cli.${currentPlatform.ext}`), [process.argv[2]], Object.assign(currentPlatform.execFileOptions,{
 	cwd: path.join(__dirname, 'bin')
 }), function (error, stdout, stderr) {
-	console.log(error,stdout,stderr);
 	if (error) {
 		console.log(error);
 	} else {
-		console.log('Deployment done.');
+		console.log(stdout);
 	}
 });
