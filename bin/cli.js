c52e8ddd966295961dc98a7b28d7655fa9cbfe91
mytharcher <mytharcher@gmail.com>
2020-02-05 21:56:47 +0800

    Fix cli env option.

--- bin/cli.js
+++ bin/cli.js
@@ -4,13 +4,13 @@ var currentPlatform = require('../env');
 
 var child_process = require('child_process');
 // console.log(process.argv[2])
-child_process.execFile(
-	path.join(__dirname, `cli.${currentPlatform.ext}`),
-	[process.argv[2]],
-	Object.assign({}, currentPlatform.execFileOptions, {
-		cwd: __dirname
-	}
-), function (error, stdout, stderr) {
+child_process.execFile(path.join(__dirname, `cli.${currentPlatform.ext}`), [
+	process.argv[2]
+],
+{
+	env: process.env,
+	cwd: __dirname
+}, function (error, stdout, stderr) {
 	if (error) {
 		console.error(error);
 	} else {
